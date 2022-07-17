e6ab64d59cf7f90ef832b21e97be975b799f18c7
iamayushy <ayushhuns@gmail.com>
2022-07-17 13:15:01 +0530

    sem

--- hyro/src/components/Hero.jsx
+++ hyro/src/components/Hero.jsx
@@ -4,6 +4,16 @@ const Hero = () => {
     return(
         <div className={hero.home}>
             <Nabvar/>
+        <div className={hero.image}>
+        <div className={hero.desc}>
+            <p>We make nature to become more natural 😁,</p>
+            <p>We are <span className={hero.log}>Acme Fresh</span></p>
+
+        </div>
+
+        <img className={hero.bgimage} src="https://img.freepik.com/free-photo/hydroponics-system-planting-vegetables-herbs-without-using-soil-health_1150-8153.jpg?w=2000"  alt="plant image" />
+
+        </div>
         </div>
     )
 }
